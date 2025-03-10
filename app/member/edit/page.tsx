3af4a2f3cc6342638d199ad2fed0b2a6554eb2ef
adenirangold <adeniranbayogold@gmail.com>
2025-03-10 21:38:22 +0100

    creating architure

---
+++ app/member/edit/page.tsx
@@ -0,0 +1,17 @@
+// "firstName":"gabttty",
+// "lastName":"sunday1234",
+// "email":"adeniranbayogold9001@gmail.com",
+
+// "phoneNumber":"08129959272",
+// "dateOfBirth":"2001-02-19T00:00:00.000Z",
+// "gender":"male",
+// "address":{
+//     "street":"Ilorin",
+//     "city":"Ilorin",
+//     "state":"Ilorin"
+// },
+// "emergencyContact":{
+//     "fullName":"Adeniran Gold",
+//     "phoneNumber":"08129959272",
+//     "relationship":"sibling"
+//
