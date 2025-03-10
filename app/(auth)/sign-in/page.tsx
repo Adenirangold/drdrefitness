617cd5fd9995979080957b5eb4463aabaaccc589
adenirangold <adeniranbayogold@gmail.com>
2025-03-10 21:21:06 +0100

    structuring page

---
+++ app/(auth)/sign-in/page.tsx
@@ -0,0 +1,10 @@
+// "email":"adeniranbayogold900@gmail.com",
+// "password":"123456789"
+
+import React from "react";
+
+const signInPage = () => {
+  return <div>signIn Page</div>;
+};
+
+export default signInPage;
