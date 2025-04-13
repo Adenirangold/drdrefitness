832a91fabd2b9c8cda4c621de70ac71263e4363d
adenirangold <adeniranbayogold@gmail.com>
2025-04-13 17:13:55 +0100

    reactify fetchdata function

--- lib/utils.ts
+++ lib/utils.ts
@@ -8,48 +8,29 @@ export function cn(...inputs: ClassValue[]) {
 
 export async function fetchData(endpoint: string, options: RequestInit = {}) {
   const url = `${config.API_KEY}${endpoint}`;
-
   try {
     const response = await fetch(url, options);
-    // console.log(response);
+
     if (!response.ok) {
-      console.error(`Error: ${response.statusText}`);
-
-      if (response.status === 409) {
-        return {
-          error:
-            "The email address is already registered. Please use a different email address.",
-        };
-      }
-      if (response.status === 401) {
-        return {
-          error: "Invalid credentials (email or password).",
-        };
-      }
-      if (response.status === 404) {
-        return {
-          error: "Token Invalid. Sign In again  ",
-        };
-      }
-
-      return {
-        error: "Something went wrong. Please try again later.",
-      };
+      const errorData = await response.json();
+      throw new Error(
+        errorData.message || "Something went wrong. Please try again later"
+      );
     }
 
     const responseData = await response.json();
 
-    if (responseData.status !== "success") {
-      return {
-        error: `${response.status || "Unknown"} error occurred. ${
-          responseData.message
-        }`,
-      };
+    if (responseData.status && responseData.status !== "success") {
+      throw new Error(
+        responseData.message || "Something went wrong. Please try again later"
+      );
     }
 
     return { data: responseData };
-  } catch (error) {
-    return { error: "Something went wrong!!. Please try again later." };
+  } catch (error: any) {
+    return {
+      error: error.message || "Something went wrong. Please try again later.",
+    };
   }
 }
 
