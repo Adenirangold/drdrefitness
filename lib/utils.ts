3918c807a2731554e80ab90843eaab69f42da0d1
adenirangold <adeniranbayogold@gmail.com>
2025-11-06 20:00:25 +0200

    iniate same id

--- lib/utils.ts
+++ lib/utils.ts
@@ -218,7 +218,6 @@ export function getSessionLabelById(data: any, currentEntry: any) {
     "Fifth Session",
     "Fifth Session",
     "Fifth Session",
-    "Fifth Session",
   ];
 
   // Step 1: Filter all sessions with same _id
